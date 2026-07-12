bcce9d8af870ab5c5f9b7ed1403944238fcc5e7a
Danilo Gomanjuk <danilogomanjuk93@gmail.com>
2026-07-12 12:41:45 +0200

    Fix repository generic types for CuttingItem and CuttingRule

--- src/main/java/alumax_erp/repository/CuttingItemRepository.java
+++ src/main/java/alumax_erp/repository/CuttingItemRepository.java
@@ -1,9 +1,10 @@
 package alumax_erp.repository;
 
+import alumax_erp.entity.CuttingItem;
 import alumax_erp.entity.WorkOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CuttingItemRepository extends JpaRepository<WorkOrder, Long> {
+public interface CuttingItemRepository extends JpaRepository<CuttingItem, Long> {
 }
